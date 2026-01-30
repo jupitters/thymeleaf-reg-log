294f61930785542e31ae18640290e5d0214c8e6c
jupitters <sampaiobale@gmail.com>
2026-01-30 13:13:46 -0300

    user service

---
+++ src/main/java/com/jupitters/RegNLog/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.jupitters.RegNLog.service.impl;
+
+import com.jupitters.RegNLog.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl  implements UserService {
+}
