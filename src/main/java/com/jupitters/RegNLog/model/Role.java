a3b6c639cbc470fe9a44b7305205c52ef4f3e16d
jupitters <sampaiobale@gmail.com>
2026-01-30 18:20:04 -0300

    user service

--- src/main/java/com/jupitters/RegNLog/model/Role.java
+++ src/main/java/com/jupitters/RegNLog/model/Role.java
@@ -19,4 +19,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    public Role(String role) {
+    }
 }
--- src/main/java/com/jupitters/RegNLog/service/impl/UserServiceImpl.java
+++ src/main/java/com/jupitters/RegNLog/service/impl/UserServiceImpl.java
@@ -1,8 +1,27 @@
 package com.jupitters.RegNLog.service.impl;
 
+import com.jupitters.RegNLog.dto.UserRegistertDto;
+import com.jupitters.RegNLog.model.Role;
+import com.jupitters.RegNLog.model.User;
+import com.jupitters.RegNLog.repository.UserRepository;
 import com.jupitters.RegNLog.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl  implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User register(UserRegistertDto request) {
+        User user = new User();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getFirstName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setRole(Arrays.asList(new Role("USER")));
+    }
 }
