e0b788794a37327d56d24a6d1f311a5d4398692c
jupitters <sampaiobale@gmail.com>
2026-01-30 12:58:09 -0300

    role model

--- src/main/java/com/jupitters/RegNLog/model/Role.java
+++ src/main/java/com/jupitters/RegNLog/model/Role.java
@@ -1,4 +1,15 @@
 package com.jupitters.RegNLog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Role {
+    private Long id;
+    private String name;
 }
--- src/main/java/com/jupitters/RegNLog/model/User.java
+++ src/main/java/com/jupitters/RegNLog/model/User.java
@@ -1,9 +1,6 @@
 package com.jupitters.RegNLog.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +19,8 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+
+    @Column(unique = true)
     private String email;
     private String password;
     private Collection<Role> role;
