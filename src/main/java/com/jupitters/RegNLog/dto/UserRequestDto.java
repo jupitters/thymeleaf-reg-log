e673f885d4420bd582bdfc809dee357473618dba
jupitters <sampaiobale@gmail.com>
2026-01-30 18:13:42 -0300

    register dto

--- src/main/java/com/jupitters/RegNLog/dto/UserRequestDto.java
+++ src/main/java/com/jupitters/RegNLog/dto/UserRequestDto.java
@@ -1,4 +1,11 @@
 package com.jupitters.RegNLog.dto;
 
+import lombok.Data;
+
+@Data
 public class UserRequestDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
