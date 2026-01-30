2c0bbcdb24b67e42f295b9afc90cb2c50dd42e51
jupitters <sampaiobale@gmail.com>
2026-01-30 11:54:19 -0300

    init

---
+++ src/main/java/com/jupitters/RegNLog/RegNLogApplication.java
@@ -0,0 +1,13 @@
+package com.jupitters.RegNLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegNLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegNLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jupitters/RegNLog/RegNLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupitters.RegNLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegNLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
