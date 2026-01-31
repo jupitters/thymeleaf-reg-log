9e773e39cd2331dc4170ccde84c037dac9ff7eef
jupitters <sampaiobale@gmail.com>
2026-01-31 16:58:45 -0300

    user controller

--- src/main/java/com/jupitters/RegNLog/controller/UserController.java
+++ src/main/java/com/jupitters/RegNLog/controller/UserController.java
@@ -1,8 +1,18 @@
 package com.jupitters.RegNLog.controller;
 
+import com.jupitters.RegNLog.dto.UserRegistertDto;
+import com.jupitters.RegNLog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    public String registerUser(@ModelAttribute("user") UserRegistertDto userDto){
+
+    }
 }
