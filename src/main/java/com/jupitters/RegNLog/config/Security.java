ae5bc6f19489ce1be7255b081b6159b8d35f8f90
jupitters <sampaiobale@gmail.com>
2026-02-02 13:03:54 -0300

    security

--- src/main/java/com/jupitters/RegNLog/config/Security.java
+++ src/main/java/com/jupitters/RegNLog/config/Security.java
@@ -1,15 +1,21 @@
 package com.jupitters.RegNLog.config;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
-public class Security {
+public class Security implements SecurityFilterChain {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -27,4 +33,13 @@ public class Security {
         return http.build();
     }
 
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        return false;
+    }
+
+    @Override
+    public List<Filter> getFilters() {
+        return List.of();
+    }
 }
