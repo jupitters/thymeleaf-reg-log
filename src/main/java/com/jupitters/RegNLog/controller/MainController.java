1b1b7856d91c3fbf6afe164b648b328f699be8b2
jupitters <sampaiobale@gmail.com>
2026-02-03 12:54:46 -0300

    main controller

---
+++ src/main/java/com/jupitters/RegNLog/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.jupitters.RegNLog.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+    public String login(){
+        return "login";
+    }
+}
