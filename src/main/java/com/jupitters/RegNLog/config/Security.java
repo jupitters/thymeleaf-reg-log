0db356a9f132c29dd3720f07572f92ffd314abbf
jupitters <sampaiobale@gmail.com>
2026-02-03 10:27:54 -0300

    security

--- src/main/java/com/jupitters/RegNLog/config/Security.java
+++ src/main/java/com/jupitters/RegNLog/config/Security.java
@@ -1,9 +1,12 @@
 package com.jupitters.RegNLog.config;
 
+import com.jupitters.RegNLog.service.UserService;
 import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -17,9 +20,19 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class Security {
+    @Autowired
+    private UserService userService;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
     }
 
     @Bean
