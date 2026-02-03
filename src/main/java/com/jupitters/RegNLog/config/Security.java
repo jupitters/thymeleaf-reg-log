d2416a783e36c1ae2dfbbc62a1ecbf582213100a
jupitters <sampaiobale@gmail.com>
2026-02-03 10:11:57 -0300

    security

--- src/main/java/com/jupitters/RegNLog/config/Security.java
+++ src/main/java/com/jupitters/RegNLog/config/Security.java
@@ -27,6 +27,16 @@ public class Security implements SecurityFilterChain {
                                 "/js/**"
                         ).permitAll()
                         .anyRequest().authenticated()
+                        .and()
+                        .formLogin()
+                        .loginPage("/login")
+                        .permitAll()
+                        .and()
+                        .logout()
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout");
                 )
                 .formLogin(Customizer.withDefaults());
 
@@ -43,5 +53,7 @@ public class Security implements SecurityFilterChain {
         return List.of();
     }
 
+    protected void configure (HttpSecurity http) throws Exception {
 
+    }
 }
