cd9ecfc5e0a17012361eeef77bbe4916cbdfdc7e
jupitters <sampaiobale@gmail.com>
2026-02-03 10:22:57 -0300

    security

--- src/main/java/com/jupitters/RegNLog/config/Security.java
+++ src/main/java/com/jupitters/RegNLog/config/Security.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
@@ -16,6 +17,11 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class Security {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
