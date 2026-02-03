4e66314e5dbd7fabed95fe98c7f56c4987bfc13f
jupitters <sampaiobale@gmail.com>
2026-02-03 10:28:51 -0300

    user service

--- src/main/java/com/jupitters/RegNLog/service/UserService.java
+++ src/main/java/com/jupitters/RegNLog/service/UserService.java
@@ -2,7 +2,8 @@ package com.jupitters.RegNLog.service;
 
 import com.jupitters.RegNLog.dto.UserRegisterDto;
 import com.jupitters.RegNLog.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User register(UserRegisterDto user);
 }
